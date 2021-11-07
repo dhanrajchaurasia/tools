f8d1411d6c498045a38f458db9490b2553212d86
dhanrajchaurasia <mnnitdhanraj@gmail.com>
2021-11-07 13:42:14 +0530

    Updated

--- JS/board.js
+++ JS/board.js
@@ -3,7 +3,14 @@ var moon = document.getElementById('moon');
 var bg = document.getElementsByClassName('area');
 var btn = document.getElementById('heading');
 var tt = document.getElementById('txt');
+var add = document.getElementById('add');
+var x = 120;
 btn.onclick = changeTheme;
+add.onclick = () => {
+    var area = document.getElementsByClassName('area');
+    x += 100;
+    area[0].style.height = `${x}vh`;
+}
 function changeTheme() {
     if (tt.innerHTML == "Dark Theme") {
         tt.innerHTML = "Light Theme";
@@ -12,6 +19,8 @@ function changeTheme() {
         bg[0].style.background = "#000";
         tt.style.color = "#000";
         btn.style.background = "#fff";
+        add.style.background = "#fff";
+        add.style.color = "#000";
     }
     else {
         tt.innerHTML = "Dark Theme";
@@ -20,5 +29,7 @@ function changeTheme() {
         bg[0].style.background = "#fff";
         tt.style.color = "#fff";
         btn.style.background = "#000";
+        add.style.background = "#000";
+        add.style.color = "#fff";
     }
 }
